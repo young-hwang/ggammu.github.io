f23a57080b0ba1e983b2abe3925e7447f90ec267
ggammu <ykcul02@gmail.com>
2019-03-01 12:57:03 +0900

    change font

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
       $(".author__urls-wrapper button").length === 0
         ? $(window).width() > 1024 // width should match $large Sass variable
         : !$(".author__urls-wrapper button").is(":visible");
-    show = false;
     if (show) {
       // fix
       $(".sidebar").addClass("sticky");
